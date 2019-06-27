9af116f6d5e6ad9d171de9cfe6f7020d539ea436
kelvin <kelvinyu2000@gmail.com>
2019-06-26 19:44:09 -0500

    implmented a very basic home page and added the models and about me js files

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Navbar extends React.Component{
+    render() {
+        return (
+            <div>
+              <ul id="nav">
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Diseases</a></li>
+                <li><a href="#">Restaurants</a></li>
+                <li><a href="#">About us</a></li>
+              </ul>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <Navbar/>
+        <div>
+        Home Page
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
+
