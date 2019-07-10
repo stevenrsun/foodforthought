8a376aa3fe986d6526239a403dfb676a73eb3888
william suh <williamsuh123@gmail.com>
2019-07-10 02:59:01 +0000

    Update tests.js

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,13 @@
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4));
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
 });
+describe('String#split', function(){
+  it('should return an array', function(){
+    assert(Array.isArray('a,b,c'.split(',')));
+  });
+})
