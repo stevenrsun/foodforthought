4e70d53000bda43e8a3b45dfeca62712c3d49b58
kelvin <kelvinyu2000@gmail.com>
2019-07-09 22:02:24 -0500

    added mocha tests

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,27 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './App';
+import getCalories from /src/pages/food.js
+import getProtein from /src/pages/food.js
+import getFat from /src/pages/food.js
+
+describe('calorie count for blueberries', () => {
+  it('works', () => {
+    let v = new Food();
+    expect(v.getCalories().toEqual(57))
+  })
+})
+
+describe('protein count for blueberries', () => {
+  it('works', () => {
+    let v = new Food();
+    expect(v.getProtein().toEqual(.7))
+  })
+})
+
+describe('fat count for blueberries', () => {
+  it('works', () => {
+    let v = new Food();
+    expect(v.getFat().toEqual(0))
+  })
+})
