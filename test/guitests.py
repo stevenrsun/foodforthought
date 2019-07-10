bb5aef93ac9c8aa6b4ff26392d0fe3bd2ab56d63
Murray Lee <murray.wulong.lee@gmail.com>
2019-07-09 20:56:27 -0500

    added selenium tests

--- test/guitests.py
+++ test/guitests.py
@@ -16,5 +16,31 @@ class Tests(unittest.TestCase):
         self.driver.get(self.url)
         self.assertIn("Food for thought", self.driver.title)
 
+        def test_home_title(self):
+        self.driver.get(self.url)
+        self.assertIn("Budget For Space", self.driver.title)
+
+    def test_food_nav(self):
+        self.driver.get(self.url + '/food')
+        link = self.driver.find_element_by_link_text("food")
+        self.assertIsNotNone(link)
+        link.click()
+        self.assertEqual("food", self.driver.current_url[-4:])
+
+    def test_restaurants_nav(self):
+        self.driver.get(self.url + '/restaurant')
+        link = self.driver.find_element_by_link_text("restaurant")
+        self.assertIsNotNone(link)
+        link.click()
+        self.assertEqual("restaurant", self.driver.current_url[-10:])
+
+    def test_diseases_nav(self):
+        self.driver.get(self.dests_url)
+        link = self.driver.find_element_by_link_text("disease")
+        self.assertIsNotNone(link)
+        link.click()
+        self.assertEqual("disease", self.driver.current_url[-7:])
+
+
 if __name__ == "__main__":
     unittest.main()
