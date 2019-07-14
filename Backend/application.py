e6bf88dffc8d6f3987401e2800b237b5b9a3c402
william suh <williamsuh123@gmail.com>
2019-07-14 00:25:57 +0000

    Update application.py

--- Backend/application.py
+++ Backend/application.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
-
+ 
 
 header_text = """
     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>"""
